4abbe575f0b5c0bcfd0846a5af7355771d124728
Vikashuvi <vikashuvi07@google.com>
2025-08-05 06:31:20 +0530

    table UI added

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,192 @@
-import Image from "next/image";
+'use client'
+
+import { userAPI } from "@/lib/app"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [user, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [showForm, setShowForm] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phonenumber: '',
+    pannumber: ''
+  })
+
+  useEffect(() => {
+    fetchUsers() // user def function
+  }, [])
+
+  const fetchUsers = async () => {
+    try {
+      setLoading(true)
+
+      const response = await userAPI.getAll()
+
+      setUsers(response.data)
+    } catch (error) {
+      console.error('error fetching', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await userAPI.create(formData)
+
+      setFormData({
+        name: '',
+        email: '',
+        phonenumber: '',
+        pannumber: ''
+      })
+
+      setShowForm(false)
+      fetchUsers()
+
+      alert('User created sucessfully')
+    } catch (error) {
+      alert('error creating')
+      console.error('error fetching', error)
+    }
+  }
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleDelete = async (id) => {
+    if (confirm('Are u sure')) {
+      try {
+        await userAPI.delete(id)
+        fetchUsers()
+
+        alert('user data deleted')
+      } catch (error) {
+        console.error('error', error)
+      }
+    }
+  }
+
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <div className="max-w-6xl mx-auto">
+
+        <div className="flex flex-row justify-between py-6"> <h1 className="text-3xl font-bold">Quick Tax</h1>
+
+          <button onClick={() => setShowForm(!showForm)} className="bg-green-500 text-white px-4 py-2 rounded ">
+            {showForm ? 'Cancel' : 'Add User'}
+          </button>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+
+        {showForm && (
+          <div>
+            <h2>Add User</h2>
+            <form onSubmit={handleSubmit} className="space-y-4">
+
+              <input
+                type="text" name='name' placeholder="Full Name" value={formData.name} onChange={handleInputChange} className="w-full p-2 border rounded-full" required
+              />
+
+              <input
+                type="email" name='email' placeholder="Email Address" value={formData.email} onChange={handleInputChange} className="w-full p-2 border rounded-full" required
+              />
+
+              <input
+                type="number" name='phonenumber' placeholder="Phone Number" value={formData.phonenumber} onChange={handleInputChange} className="w-full p-2 border rounded-full" required
+              />
+
+              <input
+                type="text" name='pannumber' placeholder="Pan Number" value={formData.pannumber} onChange={handleInputChange} className="w-full p-2 border rounded-full" required
+              />
+
+              <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded"> Create User</button>
+
+            </form>
+          </div>
+        )}
+
+        <div className="bg-white rounded-xl shadow p-6">
+          <h2 className="">All User ({user.length})</h2>
+
+          {loading ? (
+            <p className="text-gray-50">Loading users...</p>
+          ) : user.length === 0 ? (
+            <p className="text-gray-50">No users...</p>
+
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="w-full">
+                <thead className="bg-gray-50">
+                  <tr className="rounded"><th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                    ID
+                  </th>
+                    <th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                      Name
+                    </th>
+                    <th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                      Email
+                    </th>
+                    <th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                      Phone Number
+                    </th>
+                    <th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                      Pan Number
+                    </th>
+                    <th className="px-6 py-2 text-left text-md font-bold uppercase tracking-wide">
+                      Actions
+                    </th></tr>
+                </thead>
+                <tbody>
+                  {user.map(user => (
+
+                    <tr key={user.userId} className="hover:bg-gray-50">
+
+                      <td className="px-6 py-3 font-medium">
+                        {user.userId}
+                      </td>
+
+                      <td className="px-6 py-3 font-medium">
+                        {user.name}
+                      </td>
+
+                      <td className="px-6 py-3 font-medium">
+                        {user.email}
+                      </td>
+
+                      <td className="px-6 py-3 font-medium">
+                        {user.phonenumber}
+                      </td>
+
+                      <td className="px-6 py-3 font-medium">
+                        {user.pannumber}
+                      </td>
+
+                      <td className="px-6 py-3 font-medium">
+                        <button onClick={()=> handleDelete(user.userId)} className="bg-red-500 text-white px-4 py-2 rounded">Delete</button>
+                      </td>
+
+                    </tr>
+
+                  ))}
+                </tbody>
+              </table>
+
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
+
 }
